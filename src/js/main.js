36b60c815f1248a3a084685ae749e03e7d653a48
Ananev Andrei <ananevandrei11@gmail.com>
2021-07-13 13:48:25 +0400

    #4 13/07/21__13:50

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 window.onload = function () {
+	// MENU OPEN START
 	const burger = document.querySelector('.hamburger');
 	const menu = document.querySelector('.menu');
 
@@ -18,11 +19,30 @@ window.onload = function () {
 
 	document.addEventListener('click', (e) => {
 		let targetElem = e.target;
-		console.log(targetElem);
 		let goalLink1 = targetElem.classList.contains('hamburger');
 		let goalLink2 = targetElem.classList.contains('hamburger__item');
 		if (!goalLink1 && !goalLink2 && menu.classList.contains('menu-open')) {
-				menu.classList.remove('menu-open');
+			menu.classList.remove('menu-open');
 		}
 	})
+	// MENU OPEN END
+
+	// PRICE OPEN START
+	const workProduct = document.querySelectorAll('.workcost__product');
+	workProduct.forEach(element => {
+		element.addEventListener('click', (e) => {
+			let target = e.target;
+			let workInfo = element.querySelector('.workcost__info');
+			if (target.classList.contains('workcost__title')) {
+				element.classList.toggle('workcost__product-open');
+				workInfo.classList.toggle('workcost__info-open');
+			} else if (target.parentElement.classList.contains('workcost__title')) {
+				element.classList.toggle('workcost__product-open');
+				workInfo.classList.toggle('workcost__info-open');
+			} else {
+				return false;
+			}
+		})
+	});
+	// PRICE OPEN END
 }
